f3044055f91cfb2c8f52e53b35240be9b12d2348
zzy122 <1272046491@qq.com>
2019-03-01 16:46:58 +0800

    Initial Commit

---
+++ cocoapod/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  cocoapod
+//
+//  Created by zzy on 1/3/19.
+//  Copyright © 2019年 zzy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ cocoapodTests/cocoapodTests.swift
@@ -0,0 +1,34 @@
+//
+//  cocoapodTests.swift
+//  cocoapodTests
+//
+//  Created by zzy on 1/3/19.
+//  Copyright © 2019年 zzy. All rights reserved.
+//
+
+import XCTest
+@testable import cocoapod
+
+class cocoapodTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
